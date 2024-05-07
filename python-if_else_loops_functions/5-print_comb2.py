2d79ff009a6fb33ab4e40bc33c75e6d1bfea0997
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-07 16:52:36 +0000

    try

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 for i in range(0, 100):
     if i < 10:
         print("0{}".format(i), end=", ")
-    elif i >= 10 and i < 99:
-        print(i, end=", ")
+    else:
+        print("{}".format(i), end=", " if i != 99 else "\n")
