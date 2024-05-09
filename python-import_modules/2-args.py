ec17a9a1ac9be1e852373d6aef57152be559efaa
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-09 14:56:45 +0000

    try

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-len_args = len(sys.argv)
+    len_args = len(sys.argv)
 
-if len_args == 1:
-    print("0 arguments")
-elif len_args == 2:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(len_args - 1))
-    for i in range(1, len_args):
-        print("{}: {}".format(i, sys.argv[i]))
+    if len_args == 1:
+        print("0 arguments")
+    elif len_args == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(len_args - 1))
+        for i in range(1, len_args):
+            print("{}: {}".format(i, sys.argv[i]))
