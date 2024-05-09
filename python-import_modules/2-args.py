43cc3a7a8a6a9a9b3e710cdbf6f75996ccb7969f
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-09 14:54:56 +0000

    adding file 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+len_args = len(sys.argv)
+
+if len_args == 1:
+    print("0 arguments")
+elif len_args == 2:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(len_args - 1))
+    for i in range(1, len_args):
+        print("{}: {}".format(i, sys.argv[i]))
