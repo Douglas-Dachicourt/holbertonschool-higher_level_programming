6e35a30dc0e75c67bc8d8bbbad2db80683eb1323
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-13 14:14:53 +0000

    try

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    print()
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            print(matrix[i][j], end=" "
+                  if matrix[i][j] != matrix[i][len(matrix[i]) - 1] else "\n")
