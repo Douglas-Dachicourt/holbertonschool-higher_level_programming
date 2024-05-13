8e897b8cd72607a55bb720cdeeb3875ea8841ac3
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-13 16:59:06 +0000

    add file 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+
+    new_list = my_list.copy()
+    i = 0
+
+    while i < len(new_list):
+        if new_list[i] % 2 == 0:
+            new_list[i] = True
+        else:
+            new_list[i] = False
+
+        i += 1
+
+    return new_list
