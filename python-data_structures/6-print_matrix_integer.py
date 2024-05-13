b73ec51e87aca30aaedec68689f80e0ebe286be4
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-13 14:36:49 +0000

    try

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print()
+
+    if matrix == [[]]:
+        print("")
+
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end=" "
