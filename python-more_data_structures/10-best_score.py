88fda7858e435be5a88bbc05c9d13d1ee58a43a3
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-14 21:21:42 +0000

    try

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,9 @@
 def best_score(a_dictionary):
     score_max = max(a_dictionary.values())
 
+    if a_dictionary == {} or a_dictionary == "None":
+        return None
+
     for key, value in a_dictionary.items():
         if value == score_max:
             return key
