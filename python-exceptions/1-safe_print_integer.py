0a85ea96e1bf927d86d0afe795342d180c2c8d70
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-15 13:39:36 +0000

    try

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-
         if value == int(value):
             print("{:d}".format(value), end="\n")
             return True
         else:
             return False
 
-    except ValueError:
+    except:
         pass
