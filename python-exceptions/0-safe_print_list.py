8f334806bd39b0707455422e5bea8d27f119632d
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-15 13:13:59 +0000

    add folder + file 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    number = 0
+
+    try:
+        for num in range(0, x):
+
+            print(my_list[num], end="")
+            number = x
+
+    except IndexError:
+        print("")
+
+    print()
+
+    return number
