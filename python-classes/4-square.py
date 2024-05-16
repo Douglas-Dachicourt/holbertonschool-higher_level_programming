276fb66ad6d9d946e8554abdb39ff78ba0af9764
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-16 16:16:05 +0000

    add file 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Class square creation
+"""
+
+
+class Square:
+    """square class that defines a square"""
+    def __init__(self, size=0):
+        self._Square__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self._Square__size * self._Square__size)
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self._Square__size = value
