1809fc8ad88493af180818783daea52f8648141a
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-21 14:13:27 +0000

    add file 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This module provides a class that defines a rectangle
+
+"""
+
+
+class Rectangle:
+    """Class Rectangle that defines a rectangle
+
+        Args: 2
+        width = must be integer = initially set at 0
+        height = must be integer = initially set at 0
+
+
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the initial size of width"""
+        return self._Rectangle__width
+
+    @width.setter
+    def width(self, value):
+        """Set the size with a new value"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self._Rectangle__width = value
+
+    @property
+    def height(self):
+        """Retrieve the initial size of height"""
+        return self._Rectangle__height
+
+    @height.setter
+    def height(self, value):
+        """Set the size with a new value"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        self._Rectangle__height = value
