addbab0fae33ee116d713ee579bcfe19836365c6
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-21 09:02:12 +0000

    try

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module provides a function that prints two new lines
+    when a specific caracter is found in a text
+
+"""
+
+
+def text_indentation(text):
+    """
+    function : text_indentation(text)
+        Print two new lines when a specific caracter is found in a text
+
+        One argument : text
+            => MUST BE STRING ONLY
+
+    The caracters to find are : ".", "?" and ":"
+
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    else:
+        for i in range(len(text)):
+            if text[i] == ':' or text[i] == '?' or text[i] == '.':
+                print("{}".format(text[i]))
+                print("{}".format(text[i + 1]), end="\n")
+            else:
+                print("{}".format(text[i]), end="")
