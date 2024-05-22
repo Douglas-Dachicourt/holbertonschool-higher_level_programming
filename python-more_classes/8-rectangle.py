37f529c8542c9c5b9a714ea9c61816c313de73cd
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-22 09:22:52 +0000

    try

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -102,6 +102,10 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """function that compares sizes of 2 rectangles based on area"""
+
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
 
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
@@ -109,11 +113,11 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_1.area() > rect_2.area():
-            return rect_1.area()
+        if area_1 > area_2:
+            return rect_1
 
-        if rect_2.area() > rect_1.area():
-            return rect_2.area()
+        if area_2 > area_1:
+            return rect_2
 
-        if rect_1.area() == rect_2.area():
-            return rect_1.area()
+        if area_1 == area_2:
+            return rect_1
