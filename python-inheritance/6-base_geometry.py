05e91f83b450bef07c27b1b7d586dcee9112e4cc
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-22 16:25:15 +0000

    try

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This module provides a class name as BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        the main original class.
+    """
+
+    def area(self):
+        """area with the exception message"""
+        raise Exception("area() is not implemented")
