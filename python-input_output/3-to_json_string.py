beb56fb554b4c65dbcff2e225683f6aed7e7c0e5
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-27 13:56:01 +0000

    try

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import json
+
+"""
+This module is providing a function that returns an object
+(Python data structure) represented by a JSON string
+
+We have to import first the module JSON
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Function : to_json_string
+
+    Arguments :
+
+    - my_obj: the data from the json file we want to work with
+
+    Return:
+
+    The object represented by a JSON string
+
+    """
+    to_show = json.dumps(my_obj)
+
+    return to_show
