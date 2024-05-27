8403afcfa5a604723d9b9c32a365d4d2d4e5acaa
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-27 12:41:50 +0000

    try

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""
+This module provides a function to open and read
+a text file
+
+"""
+
 
 def read_file(filename=""):
+    """
+    Function read_file:
+
+    1 argument:
+
+    the way to the file we want to open and read
+
+
+    """
     with open(filename) as fic:
         content = fic.read()
         print(content)
