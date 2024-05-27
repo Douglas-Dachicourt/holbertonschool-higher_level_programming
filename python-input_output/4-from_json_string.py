4d6c79cdeba92bf144f0307833a80985db1b6d24
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-27 14:34:00 +0000

    add file 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/
+"""
+import json module
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an objetct from the json string
+    """
+    return json.loads(my_str)
