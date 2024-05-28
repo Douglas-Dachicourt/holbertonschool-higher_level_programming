a86a76af03f8d710aaf160ecb9316b49d3abbdcb
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-28 09:42:07 +0000

    try

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""import json module
+"""import os module
    import sys module
 """
-import json
+import os
 import sys
 
+"""import of load from json function """
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+"""import of save to json function """
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-try:
+if os.path.exists("add_item.json"):
     items = load_from_json_file("add_item.json")
-except FileNotFoundError:
+else:
     items = []
 
 for i in range(1, len(sys.argv)):
     items.append(sys.argv[i])
 
 save_to_json_file(items, "add_item.json")
-
-print(items)
