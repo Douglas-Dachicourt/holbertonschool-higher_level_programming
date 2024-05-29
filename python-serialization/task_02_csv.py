cdb6619e507eeabdc3c82e732b6abf5b2e3f19a8
Douglas Dachicourt <8815@holbertonstudents.com>
2024-05-29 12:51:41 +0000

    add file 02

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""import csv and json modules"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_file):
+    """
+    Function : convert_csv_to_json
+    This function is converting a csv file into a json file
+
+    Parameter:
+
+    - csv_file : the csv file to convert
+
+    Returns:
+    => True if conversion is done.
+    => False if file is not found
+
+    """
+    data = []
+    try:
+        with open(csv_file, encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            if reader:
+                for rows in reader:
+                    data.append(rows)
+                return True
+    except FileNotFoundError:
+        return False
+
+    with open("data.json", "w", encoding="utf-8") as f:
+        try:
+            json.dump(data, f, indent=0)
+        except FileNotFoundError:
+            return False
