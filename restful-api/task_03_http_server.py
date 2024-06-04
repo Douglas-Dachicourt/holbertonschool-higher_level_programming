20cd5ac3d07e8f1da3e05f1924266c1b37ecb28e
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-04 13:41:27 +0000

    try

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -84,7 +84,10 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             json_data = json.dumps(response)
             self.wfile.write(json_data.encode("utf-8"))
         else:
-            self.send_error(404, "Endpoint not found")
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
 
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler,
