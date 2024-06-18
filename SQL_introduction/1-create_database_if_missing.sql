e5973e8de385e380f7fa0506a5baf4d4e99cd937
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-18 10:09:41 +0000

    try

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- Create a database 
 
 -- Use the Create command to create a database
-CREATE database hbtn_0c_0 IF NOT EXISTS hbtn_0c_0;
+CREATE database hbtn_0c_0 IF NOT EXISTS database hbtn_0c_0;
