2fb2d871a2a693017529a7fbcfb8d4f77d2e6ade
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-18 11:56:32 +0000

    try

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,7 @@
 -- Script that create a table if not already exists
 
--- Command to create a table 
-CREATE TABLE first_table (id INT, name VARCHAR(256)) IF NOT EXISTS first_table;
+-- Select mysql database
+USE mysql
+
+-- Command to create a table
+CREATE TABLE (id INT, name VARCHAR(256)) IF NOT EXISTS first_table;
