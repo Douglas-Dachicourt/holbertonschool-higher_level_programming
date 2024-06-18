b29df9b7f190fa12953e84a8f2aa3869231fdc22
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-18 15:43:59 +0000

    add file 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Script that lists the number of records with the same score in the table second_table
 
 -- Result that displays the score + number of records for this score with the label number
-SELECT COUNT(score) AS number from second_table ORDER BY score DESC;
+SELECT COUNT(score) AS number FROM second_table ORDER BY score DESC;
