57dd5c30f2947eab403742470c8f0c5992b49bd5
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-18 14:25:31 +0000

    try

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- Script that lists all records with a score >= 10 in the table second_table
 
 -- Display both scores & names ordered by score (top first)
-SELECT score >= 10, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 AND score ORDER BY DESC;
