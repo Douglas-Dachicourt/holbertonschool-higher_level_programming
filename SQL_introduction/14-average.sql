8a675060a76a9376ab9d2cc4f069f1e4e039481e
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-18 15:13:25 +0000

    try

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Script that computes the score average of all records in the table second_table
 
 -- Command AVG to select the int column we want the average
-SELECT AVG(score) FROM second_table;
+WITH average AS SELECT AVG(score) FROM second_table;
