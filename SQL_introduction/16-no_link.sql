8b553687d68505e5c94abc081918611c10ae87c0
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-18 16:17:40 +0000

    try

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- Script that lists all records of the table second_table of the database
 
 -- Commands to
-SELECT * FROM second_table WHERE name != NULL
+SELECT score, name FROM second_table WHERE name != NULL
 ORDER BY score DESC;
