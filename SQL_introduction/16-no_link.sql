23df6b448e04ae388b75fa8798f12cc002711c30
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-18 16:20:48 +0000

    try

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- Script that lists all records of the table second_table of the database
 
--- Commands to
+-- Commands necessary when the name column is existing 
 SELECT score, name FROM second_table WHERE name IS NOT NULL
 ORDER BY score DESC;
