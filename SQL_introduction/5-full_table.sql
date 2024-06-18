84eac9e84170efe924a973cb8ac1f8c4ce0bb8cb
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-18 12:39:22 +0000

    add file 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- Script that describes a table structure
 
--- Command to select the database
-USE hbtn_0c_0;
 -- Command to show up detail of the specific table
-SHOW first_table;
+SHOW CREATE TABLE first_table;
