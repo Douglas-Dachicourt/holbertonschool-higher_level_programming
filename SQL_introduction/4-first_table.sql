2b3583a6cca22c614da60df425e47fca5ed352a9
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-18 11:44:09 +0000

    add file 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- Script that create a table if not already exists
 
 -- Command to create a table 
-CREATE TABLE IF NOT EXISTS first_table
+CREATE TABLE (id INT, name VARCHAR(256)) IF NOT EXISTS first_table;
