ac878dcbcbb31409452b099fa0e64dbb9136100c
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 09:55:11 +0000

    add file 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,8 @@
 
 -- Create a database 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Use the database
+USE hbtn_0d_2;
 -- Create a new user
 CREATE USER IF NOT EXISTS
     'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
