3933b550ddcc95e2fc0766d70b5f7fccd6cb4c39
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 11:38:14 +0000

    add file 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -8,6 +8,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id(states.id) INT NOT NULL,
+    state_id INT NOT NULL PRIMARY KEY (states.id),
     name VARCHAR(256) NOT NULL
 );
