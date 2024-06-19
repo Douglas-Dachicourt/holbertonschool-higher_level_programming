e9c20b556677d9f14ab00526ab5a29c5c7540a6e
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 09:56:53 +0000

    add file 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,10 +2,8 @@
 
 -- Create a database 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Use the database
-USE hbtn_0d_2;
 -- Create a new user
 CREATE USER IF NOT EXISTS
-    'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
+    'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Giving select privilege only to the created user
 GRANT SELECT on 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
