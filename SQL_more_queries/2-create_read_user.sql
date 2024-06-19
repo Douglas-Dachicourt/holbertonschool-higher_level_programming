2b844e9f1eecb6fedb86555ce5babf6bd49c15bc
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 10:02:44 +0000

    try

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -6,4 +6,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS
     'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Giving select privilege only to the created user
-GRANT SELECT on 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@'localhost';
