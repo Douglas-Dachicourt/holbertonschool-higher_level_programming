e3939638a9a2acd7fad701dbaa9362ea80c1ea35
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 12:16:16 +0000

    try

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,6 +5,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Use this database
 USE hbtn_0d_usa;
 -- Command 'Select' combined to 'Where' and 'Order by'
-SELECT cities.id, cities.name FROM states 
-WHERE states.name = 'California' 
-ORDER BY cities.id ASC;
+SELECT * FROM states
+WHERE states.name = 'California'
+ORDER BY states.id;
