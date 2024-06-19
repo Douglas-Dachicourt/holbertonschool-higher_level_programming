8f72846f76a4d737a8827cca395621edbdbf3dbc
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 09:30:18 +0000

    add file 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,8 @@
 -- Script that creates the MySQL server user user_0d_1
 
 -- Creation of the user if he does not exists
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' IF NOT EXISTS USER;
+CREATE USER IF NOT EXISTS 
+    'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' ;
 -- Giving all privileges on the server
 GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 -- Confirm privileges changes
