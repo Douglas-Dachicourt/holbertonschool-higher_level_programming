e71c41a0e45d1615cc087699e40cd93b9f9d35a2
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 11:25:52 +0000

    add file 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Script that creates the database 'hbtn_0d_usa' and the table 'states'
+
+-- Create the database 'hbtn_0d_usa'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use this database
+USE hbtn_0d_usa;
+-- Create table 'states' with specific options
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
