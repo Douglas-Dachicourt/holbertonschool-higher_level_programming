a8e51a2ab523e34318c90654cf5dc9065fd42ba6
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 12:05:11 +0000

    try

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,8 @@
 -- Script that lists all the cities of California that can be found in the database 'hbtn_0d_usa'
 
--- Use the database 'hbtn_0d_usa'
+-- Create database 'hbtn_0d_usa'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use this database
 USE hbtn_0d_usa;
 -- Command 'Select' combined to 'Where' and 'Order by'
 SELECT * FROM states 
