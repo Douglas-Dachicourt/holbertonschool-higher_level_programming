1e8652ea444998b5ba1048e8fad03703f08fb8fe
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 09:39:29 +0000

    add file 2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- Script that creates the database hbtn_0d_2 and the user user_0d_2
+
+-- Create a database 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create a new user
+CREATE USER IF NOT EXISTS
+    'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
+-- Giving select privilege only to the created user
+GRANT SELECT on *.* TO 'user_0d_2'@'localhost';
