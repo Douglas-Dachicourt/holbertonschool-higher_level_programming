6fd1400217d82ed4950f56d7b09c55368e865ed7
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 09:12:39 +0000

    add new folder + 1st task

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Script that shows up all the privileges of a user
+
+-- Command 'Show grants' used to see privileges of user1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Command 'Show grants' used to see privileges of user2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
