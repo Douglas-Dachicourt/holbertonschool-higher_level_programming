705c26eda4bd4118c1d4c14d0e25266b6da30611
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 13:25:31 +0000

    add file 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- Script that lists all cities contained in the database hbtn_0d_usa
 
 -- Using 'JOIN' method to get interaction between two tables
-SELECT cities.id, cities.name, states.name
-FROM cities, states
-JOIN cities.id ON cities.state_id = states.id
+SELECT cities.id, cities.name, states.name AS name
+FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id
