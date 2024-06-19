fb9e61de194d47ea24bfeb3c93522dc997f65db0
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 14:07:26 +0000

    add file 11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Script that lists all shows contained in the database hbtn_0d_tvshows
+
+-- Using 'JOIN' method
+SELECT tv_shows.title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+JOIN tv_show_genres DEFAULT NULL ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
