9abe7b8817e9b7e539118fb31c256991c0b79d01
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 11:34:11 +0000

    add file 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Script that creates the database 'hbtn_0d_usa' and the table 'cities'
+
+-- Create database 'hbtn_0d_usa'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use this database
+USE hbtn_0d_usa;
+-- Create a new table 'cities'
+CREATE TABLE IF NOT EXISTS cities
+(
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id(states.id) INT NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
