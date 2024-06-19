2485bda385d5828c41db9abf1964aacf2246581f
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-19 11:46:32 +0000

    add file 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -8,6 +8,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    FOREIGN KEY (state_id) REFERENCES (states.id) INT NOT NULL ,
-    name VARCHAR(256) NOT NULL
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
