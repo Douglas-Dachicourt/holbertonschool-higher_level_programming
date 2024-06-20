dc7b69a2e1bb2f1215b029311a1ca15f68bdda8f
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-20 12:47:20 +0000

    add new folder

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database 'hbtn_0e_0_usa'
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        password=password,
+        database=database)
+
+    c = db.cursor()
+    c.execute("""SELECT * FROM states
+    ORDER BY id""")
+    results = c.fetchall()
+
+    for row in results:
+        print(row)
