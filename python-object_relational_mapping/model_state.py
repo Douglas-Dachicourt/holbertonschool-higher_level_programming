43bd94002056131c97f34cd3a56bb638babbefbd
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-21 08:02:35 +0000

    try

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,6 +4,13 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 import MySQLdb
 
+"""
+Python file that contains the class definition of a State
+and an instance Base = declarative_base()
+
+
+"""
+
 Base = declarative_base()
 
 
