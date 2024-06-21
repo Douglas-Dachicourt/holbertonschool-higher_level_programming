bb8698b2097e32821022e3140fa9b74c255bdd49
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-21 08:09:45 +0000

    try

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,22 +1,39 @@
 #!/usr/bin/python3
 
+"""
+Module that contains the class definition of a State
+and an instance Base = declarative_base():
+
+Classes:
+    State
+
+Attributes:
+    Base: declarative_base from SQLAlchemy
+    db: connexion to the database
+
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 import MySQLdb
 
-"""
-Python file that contains the class definition of a State
-and an instance Base = declarative_base()
-
-
-"""
-
+# Using the Base for our classes (model for our DB)
 Base = declarative_base()
 
 
 class State(Base):
     """
-        Class State that inherits from Base model
+        Class State that inherits from Base
+
+        Table: state we want to establish in the DB
+
+        Attributes:
+
+        - id: integer, can not be NULL, is unique and
+        is PRIMARY KEY
+
+        - name: string, can not be NULL and is limited
+        to 128 char
 
     """
     __tablename__ = "states"
