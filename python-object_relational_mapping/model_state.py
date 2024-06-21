965379e78db7eda52f0c0850fa55265f9026e481
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-21 08:00:20 +0000

    add file model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+import MySQLdb
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        Class State that inherits from Base model
+
+    """
+    __tablename__ = "states"
+
+    id = Column(Integer, unique=True, primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+db = MySQLdb.connect(
+    host='localhost',
+    port=3306
+)
