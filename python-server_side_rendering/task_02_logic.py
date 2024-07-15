935b17656f8c9b40bb637f751266047e17b13f5b
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-15 18:36:24 +0200

    try

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -17,9 +17,13 @@ def contact():
 
 @app.route('/items')
 def items():
-    with open("items.json", "r") as file:
-        data = json.load(file)
-        return render_template('items.html', items=data['items']), 200
+    try:
+        with open("items.json", "r") as file:
+            data = json.load(file)
+            return render_template('items.html', items=data['items'])
+    except FileNotFoundError:
+        return "Error: No Json file found", 404
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
