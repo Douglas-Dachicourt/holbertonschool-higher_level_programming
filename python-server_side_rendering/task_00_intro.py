ca493a09460059e32f084ada8d65f6d1c6a1bd43
Douglas Dachicourt <8815@holbertonstudents.com>
2024-07-15 15:32:53 +0200

    try

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -7,24 +7,37 @@ def generate_invitations(template, attendees):
     
     if not isinstance(attendees, list):
         raise TypeError("Attendees must be a list of dictionaries")  
-    
+        
+    if os.path.exists("template.txt"):
+        try:
+            with open("template.txt", "r") as file:
+                template = file.read()
+        except IOError as e:
+            raise IOError(f"Error reading file : {e}")   
+
     if not template.strip():
         raise TypeError("Template is empty, no output files generated.")
 
     if not attendees:
         raise TypeError("No data provided, no output files generated.")
-      
-    if os.path.exists("template.txt"):
-        with open("template.txt", "r") as file:
-            template = file.read()
     
     for index, attendee in enumerate(attendees, start=1):
-        update = template.replace("{name}", attendee.get("name", "N/A"))
-        update = update.replace("{event_title}", attendee.get("event_title", "N/A"))
-        update = update.replace("{event_date}", attendee.get("event_date", "N/A"))
-        update = update.replace("{event_location}", attendee.get("event_location", "N/A"))
+        try:
+            update = template.replace("{name}", attendee.get("name", "N/A"))
+            update = update.replace("{event_title}", attendee.get("event_title", "N/A"))
+            update = update.replace("{event_date}", attendee.get("event_date", "N/A"))
+            update = update.replace("{event_location}", attendee.get("event_location", "N/A"))
+        except AttributeError as e:
+            raise TypeError(f"Error replacing placeholders: {e}")
 
         output_file = f"output_{index}.txt"
 
-        with open(output_file, "w") as file:
-            file.write(update) 
+
+        try:
+            if not os.path.exists(output_file):           
+                with open(output_file, "w") as file:
+                    file.write(update) 
+            else: 
+                print(f"output_file {output_file} already exists.")
+        except IOError as e:
+            raise IOError("Error to write on {output_file}: {e}")
